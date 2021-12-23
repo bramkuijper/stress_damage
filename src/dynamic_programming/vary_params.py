d98b77346371b449b2c930f85bb50e1490df2621
Kuijper <alwk201@rstudio02n.isad.isadroot.ex.ac.uk>
2021-12-23 08:53:19 +0000

    added linspace to produce a fine grid in repair

--- src/dynamic_programming/vary_params.py
+++ src/dynamic_programming/vary_params.py
@@ -3,8 +3,8 @@ import numpy as np
 import os.path, sys
 
 # specify values of the autocorrelation and risk
-autocorr = [ 0, 0.1, 0.3, 0.5, 0.7, 0.9 ]
-risk = [ 0.05, 0.1, 0.2 ]
+autocorr = [ 0, 0.1, 0.3, 0.5  ]
+risk = [ 0.05, 0.1, 0.2,0.5 ]
 
 # translate those in probabilities
 # of leaving and arriving
@@ -25,7 +25,7 @@ pAttack = [0.5]
 alpha = 1.0
 
 
-repair = [2.0,3.0,4.0,5.0]
+repair = np.linspace(start=0,stop=10,num=50)
 
 #exe = "stress_damage_lh.exe"
 exe = "stress_damage.exe"
