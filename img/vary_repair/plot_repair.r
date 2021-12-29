d8cb134e712a4cc1a763dd7edf6e09da169351f7
bramkuijper <bramkuijper@gmail.com>
2021-12-29 11:24:33 +0000

    finalized repair graph, quite alright

--- img/vary_repair/plot_repair.r
+++ img/vary_repair/plot_repair.r
@@ -4,7 +4,8 @@ library("tidyverse")
 
 type="png"
 
-data.repair <- read_csv(file="summary_repair2.csv")
+#data.repair <- read_csv(file="summary_repair2.csv")
+data.repair <- read_csv(file="summary.csv")
 
 data.repair <- filter(data.repair, repair <= 10)
 
@@ -37,26 +38,28 @@ data.repair.f$autocorr.text <- with(
   ,paste("Autocorrelation: ",autocorrelation)
 )
 
+data.repair.f$max_damage <- data.repair.f$max_damage / 1000
+
 # labels for the first row
 labels.row.1 <- data.frame(
         label=LETTERS[1:length(autocorr.u)]
         ,autocorr.text=factor(sort(unique(data.repair.f$autocorr.text)))
         ,x=rep(0.1,times=length(autocorr.u))
-        ,y=rep(0.1,times=length(autocorr.u))
+        ,y=rep(0.2,times=length(autocorr.u))
         )
 
 labels.row.2 <- data.frame(
         label=LETTERS[(length(autocorr.u)+1):(2*length(autocorr.u))]
         ,autocorr.text=factor(sort(unique(data.repair.f$autocorr.text)))
         ,x=rep(0.1,times=length(autocorr.u))
-        ,y=rep(0.1,times=length(autocorr.u))
+        ,y=rep(0.2,times=length(autocorr.u))
         )
 
 labels.row.3 <- data.frame(
         label=LETTERS[(2*length(autocorr.u)+1):(3*length(autocorr.u))]
         ,autocorr.text=factor(sort(unique(data.repair.f$autocorr.text)))
-        ,x=rep(0.1,times=length(autocorr.u))
-        ,y=rep(0.1,times=length(autocorr.u))
+        ,x=rep(1,times=length(autocorr.u))
+        ,y=rep(0.75,times=length(autocorr.u))
         )
 
 p_baseline <- ggplot(mapping=aes(x=repair, y=h_base_a)
@@ -89,7 +92,7 @@ p_peak <- ggplot(mapping=aes(x=repair, y=h_max_a)
           data = labels.row.2
           ,mapping = aes(x=x, y=y, label = label)
           ) +
-  xlab("Repair") +
+  xlab("") +
     theme(
         strip.text.x = element_blank()
         ,legend.position="none"
@@ -98,7 +101,26 @@ p_peak <- ggplot(mapping=aes(x=repair, y=h_max_a)
   ylab("Peak hormone level") +
   labs(colour="Risk")
 
-(p_baseline/p_peak)
+# plot the maximum hormone level for 
+# various amounts of repair
+p_damage <- ggplot(mapping=aes(x=repair, y=max_damage)
+       ,data=data.repair.f) +
+  geom_line(mapping=aes(colour=risk_cat)) +
+  facet_grid(. ~autocorr.text) +
+  theme_classic() +
+  geom_text(
+          data = labels.row.3
+          ,mapping = aes(x=x, y=y, label = label)
+          ) +
+  xlab("Repair") +
+    theme(
+        strip.text.x = element_blank()
+        ,legend.position="none"
+        ,panel.spacing = unit(1,"lines")
+    ) +
+  ylab("Maximum damage") +
+  labs(colour="Risk")
+(p_baseline/p_peak/p_damage)
 
 if (type == "png")
 {
